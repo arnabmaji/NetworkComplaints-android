463766d1b3b9b9fb4691f68ad49d5d073575a250
Arnab Maji <arnabmaji1999@gmail.com>
2020-01-25 15:13:14 +0530

    create ui for activity_log_in

--- app/src/main/java/io/github/arnabmaji19/networkcomplaints/LogInActivity.java
+++ app/src/main/java/io/github/arnabmaji19/networkcomplaints/LogInActivity.java
@@ -1,6 +1,7 @@
 package io.github.arnabmaji19.networkcomplaints;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,12 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+//        ActionBar actionBar = getSupportActionBar();
+//        if(actionBar != null) actionBar.hide(); //hide action bar
+    }
+
+    public void logInExistingUser(View view) {
+
     }
 }
--- app/src/main/java/io/github/arnabmaji19/networkcomplaints/WelcomeActivity.java
+++ app/src/main/java/io/github/arnabmaji19/networkcomplaints/WelcomeActivity.java
@@ -1,7 +1,10 @@
 package io.github.arnabmaji19.networkcomplaints;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
@@ -10,5 +13,22 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) actionBar.hide(); //hide action bar
+    }
+
+    public void logInUser(View view) {
+        //navigate user to Log In page
+        startActivity(new Intent(WelcomeActivity.this, LogInActivity.class)); //start LogInActivity
+        finish(); //finish the current activity
+    }
+
+    public void signUpUser(View view) {
+
+    }
+
+    public void switchOfflineMode(View view) {
+
     }
 }
