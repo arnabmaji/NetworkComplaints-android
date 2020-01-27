d423ec1d06a09daaf524f75e0eda3794903cc914
Arnab Maji <arnabmaji1999@gmail.com>
2020-01-27 21:13:41 +0530

    display user previous requests in my requests fragment

--- app/src/main/java/io/github/arnabmaji19/networkcomplaints/RequestsFragment.java
+++ app/src/main/java/io/github/arnabmaji19/networkcomplaints/RequestsFragment.java
@@ -5,15 +5,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
+import io.github.arnabmaji19.networkcomplaints.api.UserRequestsAPI;
+import io.github.arnabmaji19.networkcomplaints.model.RequestDetails;
 import io.github.arnabmaji19.networkcomplaints.util.LayoutToggler;
+import io.github.arnabmaji19.networkcomplaints.util.RequestListAdapter;
+import io.github.arnabmaji19.networkcomplaints.util.Session;
 
 public class RequestsFragment extends Fragment {
 
@@ -24,6 +34,8 @@ public class RequestsFragment extends Fragment {
     private ConstraintLayout loadRequestsLayout;
     private ConstraintLayout userRequestsLayout;
     private RecyclerView requestsRecyclerView;
+    private LinearLayout loadingBar;
+    private Button sendRequestButton;
 
     public RequestsFragment(Activity activity) {
         this.activity = activity;
@@ -40,9 +52,36 @@ public class RequestsFragment extends Fragment {
         userRequestsLayout = view.findViewById(R.id.userRequestsLayout);
         requestsRecyclerView = view.findViewById(R.id.requestsRecyclerView);
         requestsRecyclerView.setLayoutManager(new LinearLayoutManager(activity));
+        requestsRecyclerView.addItemDecoration(new DividerItemDecoration(requestsRecyclerView.getContext(), DividerItemDecoration.VERTICAL));
+        loadingBar = view.findViewById(R.id.loadingProgress);
+        sendRequestButton = view.findViewById(R.id.getRequestsButton);
 
         layoutToggler = new LayoutToggler();
         layoutToggler.addLayouts(loadRequestsLayout, userRequestsLayout);
+
+        //set loadRequestsLayout by default
+        layoutToggler.setVisible(loadRequestsLayout);
+        sendRequestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!Session.getInstance().isSessionAvailable()) { //If session is not available, user is in offline mode
+                    Toast.makeText(activity.getBaseContext(), "Oops! You are in offline mode", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                loadingBar.setVisibility(View.VISIBLE); //show the loading bar
+                UserRequestsAPI userRequestsAPI = new UserRequestsAPI();
+                userRequestsAPI.addOnCompleteListener(new UserRequestsAPI.OnCompleteListener() {
+                    @Override
+                    public void onComplete(int statusCode, List<RequestDetails> requestDetailsList) {
+                        RequestListAdapter requestListAdapter = new RequestListAdapter(requestDetailsList);
+                        requestsRecyclerView.setAdapter(requestListAdapter);
+                        layoutToggler.setVisible(userRequestsLayout);
+                    }
+                });
+                userRequestsAPI.post();
+            }
+        });
+
 
         return view;
     }
---
+++ app/src/main/java/io/github/arnabmaji19/networkcomplaints/util/RequestListAdapter.java
@@ -0,0 +1,75 @@
+package io.github.arnabmaji19.networkcomplaints.util;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import io.github.arnabmaji19.networkcomplaints.R;
+import io.github.arnabmaji19.networkcomplaints.model.RequestDetails;
+
+public class RequestListAdapter extends RecyclerView.Adapter<RequestListAdapter.RequestViewHolder> {
+
+    private List<RequestDetails> requestDetailsList;
+
+    public RequestListAdapter(List<RequestDetails> requestDetailsList) {
+        this.requestDetailsList = requestDetailsList;
+    }
+
+    @NonNull
+    @Override
+    public RequestViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //Inflate custom layout for list row
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.user_request_card, parent, false);
+        return new RequestViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RequestViewHolder holder, int position) {
+        //set texts in each row
+        RequestDetails details = requestDetailsList.get(position);
+        holder.operatorNameTextView.setText(details.getOperatorName());
+        holder.networkTypeTextView.setText(details.getNetworkType());
+        holder.signalStrengthTextView.setText(details.getStrength());
+        holder.submissionDateTextView.setText(details.getSubmissionDate());
+        //set progress status image view a/c to progress
+        if (details.getStatus() == 1) {
+            holder.progressStatusImageView.setImageResource(R.drawable.done);
+        } else {
+            holder.progressStatusImageView.setImageResource(R.drawable.not_done);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return requestDetailsList.size();
+    }
+
+    public static class RequestViewHolder extends RecyclerView.ViewHolder {
+
+        TextView operatorNameTextView;
+        TextView networkTypeTextView;
+        TextView signalStrengthTextView;
+        TextView submissionDateTextView;
+        ImageView progressStatusImageView;
+
+        public RequestViewHolder(@NonNull View view) {
+            super(view);
+
+            //link views
+            operatorNameTextView = view.findViewById(R.id.rqOperatorNameTextView);
+            networkTypeTextView = view.findViewById(R.id.rqNetworkTypeTextView);
+            signalStrengthTextView = view.findViewById(R.id.rqSignalStrengthTextView);
+            submissionDateTextView = view.findViewById(R.id.rqDateOfSubmissionTextView);
+            progressStatusImageView = view.findViewById(R.id.rqProgressStatusImageView);
+        }
+    }
+}
